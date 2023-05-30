d044cd4d0281ae313e0c1cad888fcc78b5763a41
Siddharth Borderwala <siddharth@leapwallet.io>
2023-05-30 20:43:24 +0530

    improve testing logic

--- tests/chain-store.test.ts
+++ tests/chain-store.test.ts
@@ -4,7 +4,7 @@ import { describe, it, expect } from 'vitest'
 import { SuggestChainData } from '~/lib/types'
 import { SuggestChainDataValidator } from '~/validators/chain-store'
 
-const getChainData = async () => {
+const getChainsData = async () => {
   let fileUrl = import.meta.url
   if (fileUrl.startsWith('file://')) {
     fileUrl = fileUrl.slice(7)
@@ -12,32 +12,47 @@ const getChainData = async () => {
   const fileDir = dirname(fileUrl)
   const dirPath = join(fileDir, '../data/chain-store')
   const dirData = await readdir(dirPath)
-  const chainData: SuggestChainData[] = await Promise.all(
-    dirData.map(async (file) => {
-      const data = await readFile(join(dirPath, file), 'utf-8')
-      return JSON.parse(data)
-    })
-  )
+  const chainsData: SuggestChainData[] = await Promise.all(
+    dirData
+      .filter((file) => file.endsWith('.json'))
+      .map(async (file) => {
+        const data = await readFile(join(dirPath, file), 'utf-8')
+        try {
+          return JSON.parse(data)
+        } catch (error) {
+          console.error(
+            `⚠️ ERROR: Failed to parse JSON data for "${file}"\n${error}`
+          )
+          return null
+        }
+      })
+  ).then((data) => data.filter((item) => item !== null))
 
-  return chainData
+  return chainsData
 }
 
 describe('validate chain store data', async () => {
-  const chainData = await getChainData()
+  const chainsData = await getChainsData()
 
-  if (chainData.length === 0) {
+  if (chainsData.length === 0) {
     it('should validate data for empty chain store', () => {
       console.warn('⚠️ WARNING: No chains data found for chain-store')
       expect(true).toBe(true)
     })
   }
 
-  chainData.forEach((data) => {
-    it(`should validate data for ${data.chainId}`, () => {
+  chainsData.forEach((data) => {
+    it(`should validate data for "${data.chainId}"`, () => {
       const validationResult = SuggestChainDataValidator.safeParse(data)
       // print out the error message if validation fails
       if (!validationResult.success) {
-        console.error(JSON.stringify(validationResult.error, null, 2))
+        console.error(
+          `⚠️ VALIDATION ERROR:\n${JSON.stringify(
+            validationResult.error.issues,
+            null,
+            2
+          )}`
+        )
       }
       // test case - validation should pass
       expect(validationResult.success).toBe(true)
