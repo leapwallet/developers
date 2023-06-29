7211ae344cca9b32db68c53c10208a99165d0973
Siddharth Borderwala <siddharth@leapwallet.io>
2023-06-29 11:19:07 +0530

    github pages redirect

--- src/components/page-banner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const PageBanner: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return (
-    <div className="absolute top-0 left-0 bg-indigo-300 w-full flex items-center justify-center py-1">
-      {children}
-    </div>
-  )
-}
